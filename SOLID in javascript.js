374f68246a7fc19cccc2f9dcea935984d0f00f79
Kevin Cho <hcho163@aucklanduni.ac.nz>
2020-03-23 20:59:41 +1300

    added js single responsibility

--- SOLID in javascript.js
+++ SOLID in javascript.js
@@ -1,1 +1,22 @@
-/* 
+// SINGLE RESPONSIBILITY PRINCIPLE
+// A class should have one, and only one, reason to change
+
+// Bad approach
+function createUserBadWay(name, email) {
+    let isEmailValid = validateEmail(email);
+    if (isEmailValid) {
+        createUserInDatabase(name, email);
+    }
+}
+
+// Good approach
+function createUserGoodWay(name, email) {
+    createUserInDatabase(name, email);
+}
+
+function validateRequest(req) {
+    let isEmailValid = validateEmail(req.mail);
+    if (isEmailValid) {
+        createUserGoodWay(req.name, req.email);
+    }
+}
