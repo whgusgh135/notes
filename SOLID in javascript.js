6ffb9146a081ad1b63299f62bc5630308bd6a112
Kevin Cho <hcho163@aucklanduni.ac.nz>
2020-03-25 21:19:39 +1300

    added Liskov substitution in javascript

--- SOLID in javascript.js
+++ SOLID in javascript.js
@@ -50,3 +50,49 @@ let cities = {
         Object.keys(this.items).forEach(key => {console.log(this.items[key])})
     }
 }
+
+
+// LISKOV SUBSTITUTION PRINCIPLE
+// Any class that is the child of a parent class should be usable
+// in place of its parent without any unexpected behaviour
+class Employee {
+    constructor(name, id, department, salary) {
+        this.name = name;
+        this.id = id;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    getEmployeeDetails() {
+        console.log('Employee Name: ', this.name);
+        console.log('Employee id: ', this.employee_id);
+        console.log('Department is: ', this.department);
+    }
+
+    getSalaryDetails() {
+        console.log('Basic Salary is: ', this.basic_salary);
+    }
+}
+
+class Permanent extends Employee {
+    constructor(name, id, department, designation, salary, tds){
+        super(name, id, department, salary);
+        this.type = 'Permanent';
+        this.designation = designation;
+        this.tds = tds;
+    }
+
+    getOtherDetails() {
+        console.log('Department is: ', this.designation);
+        console.log('Employee type is: ', this.type);
+    }
+
+    getSalaryDetails() {
+        console.log('Basic salary is: ', this.basic_salary);
+        console.log('TDS is: ', this.tds);
+    }
+}
+// Derived class just extension of Base class without replacing the functionality of old class
+// Employee class have getSalaryDetails() and Permanent, Contract classes also have getSalaryDetails() function.
+// Means Base class getSalaryDetails() behaviour is changed by the Subtype classes(derived class).
+// It should not be done according to the LSP
